bca405e0949ea9d60ae80646c05ce20d88b6c473
Quy Nguyen <imnnquy@gmail.com>
2018-11-25 01:21:20 +0800

    Add profile build

---
+++ precache-manifest.79124d91035a3d1bd987f8d6166e3198.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b6b368ba0ebc23f5741aa566359853e",
+    "url": "/static/media/reactlogo.5b6b368b.png"
+  },
+  {
+    "revision": "2c4c765d310c20d6eb0d3e03a8130889",
+    "url": "/static/media/quy.2c4c765d.jpg"
+  },
+  {
+    "revision": "9b598ec86b790d45ed1cad1ebf656f01",
+    "url": "/static/media/background.9b598ec8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de75a40b007ee4582e99",
+    "url": "/static/js/main.de75a40b.chunk.js"
+  },
+  {
+    "revision": "6c728c3c7e9dc1dc611a",
+    "url": "/static/js/1.6c728c3c.chunk.js"
+  },
+  {
+    "revision": "de75a40b007ee4582e99",
+    "url": "/static/css/main.3c73e297.chunk.css"
+  },
+  {
+    "revision": "6c728c3c7e9dc1dc611a",
+    "url": "/static/css/1.3d961936.chunk.css"
+  },
+  {
+    "revision": "640ee304e0f014a8198fc171b15396d7",
+    "url": "/index.html"
+  }
+];
