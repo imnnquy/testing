da2259b84e9b7a18a06dd5cc06744594e57a15c4
Quy Nguyen <imnnquy@gmail.com>
2018-11-25 01:23:13 +0800

    Change path

---
+++ precache-manifest.78edf8faa0e37289bbd7897df361157a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b6b368ba0ebc23f5741aa566359853e",
+    "url": "/testing/static/media/reactlogo.5b6b368b.png"
+  },
+  {
+    "revision": "2c4c765d310c20d6eb0d3e03a8130889",
+    "url": "/testing/static/media/quy.2c4c765d.jpg"
+  },
+  {
+    "revision": "9b598ec86b790d45ed1cad1ebf656f01",
+    "url": "/testing/static/media/background.9b598ec8.jpg"
+  },
+  {
+    "revision": "47854e5355f3c459be53",
+    "url": "/testing/static/js/runtime~main.47854e53.js"
+  },
+  {
+    "revision": "de75a40b007ee4582e99",
+    "url": "/testing/static/js/main.de75a40b.chunk.js"
+  },
+  {
+    "revision": "6c728c3c7e9dc1dc611a",
+    "url": "/testing/static/js/1.6c728c3c.chunk.js"
+  },
+  {
+    "revision": "de75a40b007ee4582e99",
+    "url": "/testing/static/css/main.3c73e297.chunk.css"
+  },
+  {
+    "revision": "6c728c3c7e9dc1dc611a",
+    "url": "/testing/static/css/1.3d961936.chunk.css"
+  },
+  {
+    "revision": "78770cf71ce18e0d7c06d81171a03111",
+    "url": "/testing/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79124d91035a3d1bd987f8d6166e3198.js"
+  "/testing/precache-manifest.78edf8faa0e37289bbd7897df361157a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/testing/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
